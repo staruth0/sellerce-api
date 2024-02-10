953140e0ca5662e265580f08f68727399040b385
fabriceMokfembam <fabricemokfembam@.com>
2024-02-10 08:23:44 -0800

    updated some models as per UI reqirements

--- models/testimonial.model.mjs
+++ models/testimonial.model.mjs
@@ -5,6 +5,10 @@ const testimonialSchema = new mongoose.Schema({
         type: String,
         required:true,
     },
+    image: {
+        type: String,
+        required:true
+    },
     user_id: {
         type: String,
         required: true
